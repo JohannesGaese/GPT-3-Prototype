a3eac00a2a35d8cc0597cf93f64f4b28b29513f5
Johannes Gaese <jonnygaese@gmail.com>
2021-04-16 19:08:20 +0200

    upload

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,12 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import "react-mde/lib/styles/css/react-mde-all.css";
 
-import { FilterProvider } from "./context/FilterContext";
 
 import BackendRoute from "./components/routing/BackendRoute";
 
 import VisNavbar from "./components/layout/VisNavbar";
 
 import Home from "./components/pages/Home";
-import Reporting from "./components/pages/Reporting"
-import DonationInputPanel from "./components/pages/DonationInputPanel"
-import HomeToShow from "./components/pages/HomeToShow";
-import LandingPage from "./components/pages/LandingPage";
 
 import { withRouter } from "react-router-dom";
 
@@ -21,21 +16,15 @@ import { withRouter } from "react-router-dom";
 function App({ location }) {
   return (
     <Fragment>
-      <FilterProvider>
         <div className="App">
           <BrowserRouter>  
-            <Route exact path="/" component={LandingPage} />
             <VisNavbar/>
-          
-            <Switch>             
-              <Route exact path="/home" component={HomeToShow} />
-              <Route exact path="/database" component={Home} />
-              <Route exact path="/reporting" component={Reporting} />
-              <Route exact path="/impactcalculator" component={DonationInputPanel} />
+            <Switch>
+              <Route exact path="/" component={Home} />             
+              
             </Switch>
             </BrowserRouter>
         </div>
-      </FilterProvider>
     </Fragment>
 
 
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -2,14 +2,7 @@ import React, { useEffect, useContext, useState } from "react";
 import useSWR from "swr";
 import Fuse from "fuse.js";
 
-import { FilterContext, FilterProvider } from "../../context/FilterContext";
-
-import Filter from "../layout/feed/Filter";
-import DonationCard from "../layout/feed/DonationCard";
-
 import axios from "axios";
-
-import money from "./../../assests/icons/money.png"
 
 import {
     Container,
@@ -41,78 +34,6 @@ const x = axios.get("/api/donations/allCards")
     .then(res => console.log(res.data))
 console.log(x)
 
-axios.get('https://unstats.un.org/SDGAPI/v1/sdg/Indicator/List')
-  .then(res => console.log(res.data))
-
-  const dummySet = [
-    {
-        _id: 1,
-        titel: "School in Yemen",
-        orgName: "UNICEF",
-        orgDescription: "Improve Access to Education",
-        donationDescribtion: "In cooperation with Unicef the Deutsche Börse Group was able to build a school for 300 children",
-        amount: "8000 USD",
-        paymentFrequency: "Yearly",
-        partnerShip: "Partner since 2020",
-        projectStart: "29.11.2020",
-        impact: {
-          achieved: "300 students entered the school",
-          hciDynamic: "0.32",
-          impactScore: "3.8",
-          
-
-        },
-        country: "Yemen",
-        linkToOrganisation: "https://www.unicef.org",
-        esgGoal: "Social",
-        sustainableGoal: "Quality education",
-        
-    },
-    {
-      _id: 2,
-      titel: "Access in Modern Energy Cooking",
-      orgName: "World Bank",
-      orgDescription: "Progress towards ensuring access to modern cooking solutions",
-      donationDescribtion: "In cooperation with Unicef the Deutsche Börse Group was able to increase access to modern energy cooking by 2,5% in Sub-Saharan Africa",
-      amount: "20000 USD",
-      paymentFrequency: "Monthly",
-      partnerShip: "Partner since 2020",
-      projectStart: "01.10.2020",
-      impact: {
-        achieved: "150 stoves embedded",
-        impactScore: "3.6",
-      },
-      country: "Ghana",
-      linkToOrganisation: " https://www.worldbank.org/en/topic/energy/publication/the-state-of-access-to-modern-energy-cooking-services",
-      esgGoal: "Environmental",
-      sustainableGoal: "Affordable and Clean Energy",
-
-    },
-
-    {
-      _id: 3,
-      titel: "Peace, Justice and Strong Institutions",
-      orgName: "The Open Working Group",
-      orgDescription: "Progress towards ensuring access to modern cooking solutions",
-      donationDescribtion: "In cooperation with the OWG the Deutsche Börse Group was able to help establish the system in which the SDGs can provide incentives for positive change, built around carrots and sticks and based on subsidiarity",
-      amount: "38000 USD",
-      paymentFrequency: "Yearly",
-      partnerShip: "Partner since 2020",
-      projectStart: "01.12.2020",
-      impact: {
-        achieved: "sub-goals classification system created",
-        impactScore: "2.8",
-      },
-      country: "Jordan",
-      linkToOrganisation: " https://sustainabledevelopment.un.org/sdgsproposal.html",
-      esgGoal: "Governance",
-      sustainableGoal: " Peace, Justice and Strong Institutions ",
-      
-  },
-
-  
-];
-
 
 const Home = () => {
 
@@ -125,10 +46,6 @@ const Home = () => {
   },[donations])
   console.log(donations)
 
-    const {
-      getFilteredDonationCards,
-      cardFilter
-    } = useContext(FilterContext)
 
     const [displayDonations, setDisplayDonations] = useState([]);
     const [filteredDonations, setFilteredDonations] = useState([]);
@@ -180,8 +97,6 @@ const Home = () => {
         setPageSize(parseInt(e.target.pageSize.value));
     };
 
-    const [data, SetData] = useState(dummySet);
-
     const settingFilter = () => {
       return
     };
@@ -194,133 +109,7 @@ const Home = () => {
     return (
         <>
             <Container  fluid className="text-center pt-3" /*style={{backgroundColor: "#e4d6ff"}}*/>
-              <h3 className="mb-5 mt-3">See past projects of other users <OverlayTrigger
-                                  placement="right"
-                                  overlay={<Tooltip id={`tooltip-2`}>
-                                  Need help with the <strong>Donation Database</strong>
-                                </Tooltip>}
-                                >
-                                    <Button
-                                      variant="Vis1"
-                                      
-                                      onClick={handleShowFeed}
-                                    >
-                                      Donation Database
-                                    </Button>
-                                  
-                                </OverlayTrigger>
-                                <Modal
-                                      show={showFeed}
-                                      onHide={handleCloseFeed}
-                                      backdrop="dynamic"
-                                      keyboard={false}
-                                      
-                                      centered
-                                      >
-                                        <Modal.Header closeButton>
-                                          <Modal.Title className="text-center"> Donation Database </Modal.Title>
-                                        </Modal.Header>
-                                        <Modal.Body>
-                                          <Row className="justify-content-md-center mt-2 mb-4 mx-2">
-                                            <div  style={{color: "grey", fontSize: "large"}}>
-                                              You want to know what the impact was of other Projects? 
-                                            </div>
-                                          </Row>
-                                          <Image src={money} />
-                                          <Row className="justify-content-md-center mt-5 mx-2">
-                                            <div style={{color: "grey", fontSize: "large"}}>
-                                              Use the Donation Database to get a feeling for impact various donations can achieve.
-                                            </div>
-                                          </Row>
-                                          <Row className="justify-content-md-center mt-3 mx-2">
-                                            <div style={{color: "grey", fontSize: "large"}}>
-                                              Find out which SDGs are project worthy and concentrate your donation decisions on the important projects.
-                                            </div>
-                                          </Row>
-                                          <Row className="justify-content-md-center mb-2 mt-3 mx-2">
-                                            <div style={{color: "grey", fontSize: "large"}}>
-                                              Update the Donation Database page with your projects and help others with their project decisions.
-                                            </div>
-                                          </Row>
-                                        </Modal.Body>
-                                      </Modal> </h3>
               
-              <Row className="mt-5 justify-content-md-center mb-5">
-                
-                <Col>
-                <Button
-                    variant="Vis1"
-                    style={{width: "200px", height: "55px"}}
-                    size="lg"
-                    onClick={(e) => {
-                      data.length === 3 ?
-                      SetData([data[0]])
-                      : data[0].esgGoal === "Social"
-                      ? SetData(dummySet)
-                      : SetData([dummySet[0]])}}
-                    >Social</Button>
-                </Col>
-                <Col className="align-items-center" >
-                  <Button
-                    variant="Vis1"
-                    style={{width: "200px", height: "55px"}}
-                    size="lg"
-                    onClick={(e) => {
-                      data.length === 3  ?
-                      SetData([data[1]])
-                      : data[0].esgGoal === "Environmental"
-                      ? SetData(dummySet)
-                      : SetData([dummySet[1]])}}
-                    >Environmental</Button>
-                  
-                </Col>
-                <Col>
-                <Button
-                    variant="Vis1"
-                    style={{width: "200px", height: "55px"}}
-                    size="lg"
-                    onClick={(e) => {
-                      data.length === 3 ?
-                      SetData([data[2]])
-                      : data[0].esgGoal === "Governance"
-                      ? SetData(dummySet)
-                      : SetData([dummySet[2]])}}
-                    >Governance</Button>
-                </Col>
-                
-              </Row>
-              <hr></hr>
-              <Row className="mt-5 justify-content-md-center" >
-                
-              
-                {data  ? (
-                  <>
-                    <Col lg = {8} md={8} className="justify-content-md-center">
-                    {data
-                      .slice(page * pageSize, 3)
-                      .map((donation) => (
-                        <Col /*style={{backgroundColor : "grey"}}*/ className="pt-3 px-3 pb-3">
-                          
-                        <DonationCard
-                          
-                          className="shadow-sm "
-                          key={donation._id}
-                          donation={donation}
-                        />
-                        </Col>
-                      ))}
-                    </Col>
-                    
-                  
-                    
-                  </>
-                ) : (
-                  <div className="text-AO1 text-center spinner">
-                    <Spinner animation="grow"></Spinner>
-                    <h4>Cards are being loaded</h4>
-                  </div>
-                )}
-              </Row>
             </Container>
         </>
     );
